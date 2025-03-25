6daae7ffefcb0dd030273c7cfb57e1f2b233bbe9
FedivStanislav <fediv.stanislav@comp-sc.if.ua>
2025-03-25 10:08:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ let driver = {
     personalLimitations: "No driving at night"
 };
 
-let car1 = {
-    color: "black",
-    maxSpeed: "256",
-    driver: driver,
-    tuning: true,
-    numberOfAccidents: 0,
-    drive: function() {
-        console.log("I am not driving at night.");
-    }
+let car1 = new Object();
+car1.color = "black";
+car1.maxSpeed = 256;
+car1.driver = new Object();
+car1.driver.name = "Stas Fediv";
+car1.driver.category = "C";
+car1.driver.personalLimitations = "No driving at night";
+car1.tuning = true;
+car1.numberOfAccidents = 0;
+car1.drive = function() {
+    console.log("I am not driving at night.");
 };
 
 console.log(car1);
